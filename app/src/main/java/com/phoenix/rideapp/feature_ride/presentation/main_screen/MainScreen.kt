bd62e2658e0dadbf842c80c5992ec84742122105
vs-machado <vinicius.s.machado@protonmail.com>
2024-12-11 11:33:41 -0300

    Os layouts das tres telas foram ajustados para considerar o padding das system bars.

--- app/src/main/java/com/phoenix/rideapp/feature_ride/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/com/phoenix/rideapp/feature_ride/presentation/main_screen/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.outlined.Home
@@ -68,7 +69,9 @@ fun MainScreen(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
     ) {
         Text(
             text = "Olá, passageiro!",
--- app/src/main/java/com/phoenix/rideapp/feature_ride/presentation/ride_prices_screen/RidePricesScreen.kt
+++ app/src/main/java/com/phoenix/rideapp/feature_ride/presentation/ride_prices_screen/RidePricesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -97,6 +98,7 @@ fun RidePricesScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .systemBarsPadding()
             .padding(bottom = 16.dp)
     ) {
         Spacer(modifier = Modifier.padding(8.dp))
